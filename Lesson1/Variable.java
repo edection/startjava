759a2dadc22b549ca332794a1fb468118f6f9d29
edection <edection@gmail.com>
2020-10-26 16:08:09 +0300

    bug fixes myfirstapp part 01

--- Lesson1/Variable.java
+++ Lesson1/Variable.java
@@ -1,22 +1,23 @@
 public class Variable {
-	public static void main(String[] args) {
-	byte windowsVersion = 10;
-	short pcRAM = 8;
-	int amdRadeonModel = 5450;
-	long copyBrotherModel = 1610;
-	float ghz = 2.7f;
-	double smbiosVersion = 2.6;
-	char email = '@';
-	boolean checkEthernet = true;
-	boolean checkWifi = false;
-		System.out.println("windowsVersion =" + windowsVersion);
-		System.out.println("pcRAM =" + pcRAM);
-		System.out.println("amdRadeonModel =" + amdRadeonModel);
-		System.out.println("copyBrotherModel =" + copyBrotherModel);
-		System.out.println("ghz =" + ghz);
-		System.out.println("smbiosVersion =" + smbiosVersion);
-		System.out.println("email =" + email);
-		System.out.println("checkEthernet =" + checkEthernet);
-		System.out.println("checkWifi =" + checkWifi);
+		public static void main(String[] args) {
+		byte windowsVersion = 10;
+		short pcRAM = 8;
+		int amdRadeonModel = 5450;
+		long copyBrotherModel = 1610;
+		float ghz = 2.7f;
+		double smbiosVersion = 2.6;
+		char email = '@';
+		boolean checkEthernet = true;
+		boolean checkWifi = false;
+		
+		System.out.println ("windowsVersion =" + windowsVersion);
+		System.out.println ("pcRam =" + pcRAM);
+		System.out.println ("amdRadeonModel =" + amdRadeonModel);
+		System.out.println ("copyBrotherModel =" + copyBrotherModel);
+		System.out.println ("ghz =" + ghz);
+		System.out.println ("smbiosVersion =" + smbiosVersion);
+		System.out.println ("email =" + email);
+		System.out.println ("checkEthernet =" + checkEthernet);
+		System.out.println ("checkWifi =" + checkWifi);
 	}
 }
