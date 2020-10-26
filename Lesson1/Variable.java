e0ce223be02ed23197eb5abafe6ccfe54294bb8f
edection <edection@gmail.com>
2020-10-26 19:44:26 +0300

    bug fixes myfirstapp variable part 03

--- Lesson1/Variable.java
+++ Lesson1/Variable.java
@@ -1,5 +1,5 @@
 public class Variable {
-		public static void main(String[] args) {
+	public static void main(String[] args) {
 		byte windowsVersion = 10;
 		short pcRam = 8;
 		int amdRadeonModel = 5450;
@@ -10,14 +10,14 @@ public class Variable {
 		boolean checkEthernet = true;
 		boolean checkWifi = false;
 
-		System.out.println ("windowsVersion =" + windowsVersion);
-		System.out.println ("pcRAM =" + pcRam);
-		System.out.println ("amdRadeonModel =" + amdRadeonModel);
-		System.out.println ("copyBrotherModel =" + copyBrotherModel);
-		System.out.println ("ghz =" + ghz);
-		System.out.println ("smbiosVersion =" + smbiosVersion);
-		System.out.println ("email =" + email);
-		System.out.println ("checkEthernet =" + checkEthernet);
-		System.out.println ("checkWifi =" + checkWifi);
+		System.out.println("windowsVersion = " + windowsVersion);
+		System.out.println("pcRAM = " + pcRam);
+		System.out.println("amdRadeonModel = " + amdRadeonModel);
+		System.out.println("copyBrotherModel = " + copyBrotherModel);
+		System.out.println("ghz = " + ghz);
+		System.out.println("smbiosVersion = " + smbiosVersion);
+		System.out.println("email = " + email);
+		System.out.println("checkEthernet = " + checkEthernet);
+		System.out.println("checkWifi = " + checkWifi);
 	}
 }
