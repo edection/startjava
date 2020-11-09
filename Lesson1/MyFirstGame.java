bad5747c4fb5f6c892dddc9b7b7271d834b19571
edection <edection@gmail.com>
2020-11-09 13:32:41 +0300

    MyFirstGame.java - bug fixes v2

--- Lesson1/MyFirstGame.java
+++ Lesson1/MyFirstGame.java
@@ -1,14 +1,14 @@
 public class MyFirstGame {
 	public static void main(String[] args) {
 		int computerNum = 10;// компьютер загадывает целое число от 0 до 100
-		int player = 15; // число игрока
+		int player = 10; // число игрока
 
 		if(player < computerNum) {
-			do{
+			while(player != computerNum) {
 				System.out.println(player + " Your number is less than the computer's number");
 				player++;
-			} while(player != computerNum);
-			} else if(player > computerNum) {
+			}
+		} if(player > computerNum) {
 			while(player != computerNum) {
 				System.out.println(player + " Your number is greater than the computer's number");
 				player--;
