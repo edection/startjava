ea1517419312fb41dcb9f11b99872a005d615ea5
edection <edection@gmail.com>
2020-11-12 14:29:13 +0300

    Person.java

---
+++ Lesson2/Person.java
@@ -0,0 +1,25 @@
+public class Person {
+	
+	String personName = "Super Mario"; // имя
+	String personGender = "Male"; // пол
+	int age = 25; // возраст
+	float height = 1.5f; // рост
+	int weight = 60; // вес
+
+	void move() {
+
+	}
+
+	void jump() {
+
+	}
+
+	void run() {
+
+	}
+
+	void search() {
+
+	}
+
+}
