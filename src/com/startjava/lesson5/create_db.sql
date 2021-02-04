b7161f14743bcc308f2ac5c247b864d38576a582
edection <edection@gmail.com>
2021-02-04 14:02:01 +0300

    Update 1 Lesson 5 Jaegers

--- src/com/startjava/lesson5/create_db.sql
+++ src/com/startjava/lesson5/create_db.sql
@@ -1,6 +1,17 @@
+--psql -U postgres --file=create_db.sql
+
 CREATE DATABASE Jaegers;
 
 \c jaegers;
 
 CREATE TABLE Jaegers (
-id SERIAL PRIMARY KEY, modelName TEXT, mark CHAR(6), height REAL, weight REAL, status TEXT, origin TEXT, kaijuKill INTEGER, launch DATE);
+    id SERIAL PRIMARY KEY,
+    modelName TEXT,
+    mark CHAR(6),
+    height REAL,
+    weight REAL,
+    status TEXT,
+    origin TEXT,
+    kaijuKill INTEGER,
+    launch DATE)
+;
--- src/com/startjava/lesson5/queries.sql
+++ src/com/startjava/lesson5/queries.sql
@@ -1,16 +1,20 @@
+--psql -U postgres --file=queries.sql
+
+\c jaegers;
+
 SELECT * FROM Jaegers;
 
-SELECT modelName, mark, height, weight, status, origin, kaijuKill, launch FROM Jaegers WHERE status = 'Active';
+SELECT * FROM Jaegers WHERE status = 'Active';
 
-SELECT modelName, mark, height, weight, status, origin, kaijuKill, launch FROM Jaegers WHERE mark = 'Mark-1' OR mark = 'Mark-6';
+SELECT * FROM Jaegers WHERE mark = 'Mark-1' OR mark = 'Mark-6';
 -- или вариант:
-SELECT modelName, mark, height, weight, status, origin, kaijuKill, launch FROM Jaegers WHERE mark SIMILAR TO '(Mark-1|Mark-6)';
+-- SELECT * FROM Jaegers WHERE mark SIMILAR TO '(Mark-1|Mark-6)';
 
-SELECT modelName, mark, height, weight, status, origin, kaijuKill, launch FROM Jaegers ORDER BY mark DESC;
+SELECT * FROM Jaegers ORDER BY mark DESC;
 
-SELECT modelName, mark, height, weight, status, origin, kaijuKill, launch FROM Jaegers WHERE launch = (SELECT MIN(launch) FROM Jaegers);
+SELECT * FROM Jaegers WHERE launch = (SELECT MIN(launch) FROM Jaegers);
 
-SELECT modelName, mark, height, weight, status, origin, kaijuKill, launch FROM Jaegers
+SELECT * FROM Jaegers
 	WHERE kaijuKill = (SELECT MIN(kaijuKill) FROM Jaegers) 
 	OR kaijuKill = (SELECT MAX(kaijuKill) FROM Jaegers);
 
@@ -18,4 +22,4 @@ SELECT AVG(weight) FROM Jaegers;
 
 UPDATE Jaegers SET kaijuKill = kaijuKill + 1 WHERE status = 'Active';
 
-DELETE FROM Jaegers WHERE status ='Active';
+DELETE FROM Jaegers WHERE status ='Destroyed';
